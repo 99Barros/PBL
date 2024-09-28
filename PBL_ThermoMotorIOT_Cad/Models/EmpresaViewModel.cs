e98d761d016b4d975a2e61cebf50d3bab9e25482
Guilherme  Santos <guispinolas@gmail.com>
2024-09-28 16:16:24 -0300

    Add ViewModels, FIX sobre e Login

---
+++ PBL_ThermoMotorIOT_Cad/Models/EmpresaViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PBL_ThermoMotorIOT_Cad.Models
+{
+    public class EmpresaViewModel
+    {
+        public int IdEmpresa { get; set; }
+
+        [Required(ErrorMessage = "Nome da empresa é obrigatório.")]
+        [StringLength(150, ErrorMessage = "O nome da empresa deve ter no máximo 150 caracteres.")]
+        public string NomeEmpresa { get; set; }
+
+        [Required(ErrorMessage = "CNPJ é obrigatório.")]
+        [StringLength(20, ErrorMessage = "O CNPJ deve ter no máximo 20 caracteres.")]
+        public string CNPJ { get; set; }
+
+        [Required(ErrorMessage = "Endereço é obrigatório.")]
+        [StringLength(255, ErrorMessage = "O endereço deve ter no máximo 255 caracteres.")]
+        public string Endereco { get; set; }
+
+        [StringLength(20, ErrorMessage = "O telefone deve ter no máximo 20 caracteres.")]
+        public string Telefone { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        [StringLength(100, ErrorMessage = "O email deve ter no máximo 100 caracteres.")]
+        public string Email { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+    }
+
+
+}
---
+++ PBL_ThermoMotorIOT_Cad/Models/UsuarioViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PBL_ThermoMotorIOT_Cad.Models
+{
+    public class UsuarioViewModel
+    {
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "Usuário é obrigatório.")]
+        [StringLength(50, ErrorMessage = "O usuário deve ter no máximo 50 caracteres.")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatória.")]
+        [StringLength(255, ErrorMessage = "A senha deve ter no máximo 255 caracteres.")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        [StringLength(100, ErrorMessage = "O email deve ter no máximo 100 caracteres.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DataNascimento { get; set; }
+
+        [StringLength(20, ErrorMessage = "O telefone deve ter no máximo 20 caracteres.")]
+        public string Telefone { get; set; }
+
+        public DateTime DataRegistro { get; set; }
+    }
+
+}
