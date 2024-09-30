ea25796d0d2231443015ac561f75b00cff1e665b
Guilherme  Santos <guispinolas@gmail.com>
2024-09-29 21:39:36 -0300

    Trocando Conexao por padrão da FESA

--- PBL_ThermoMotorIOT_Cad/DAO/ConexaoBD.cs
+++ PBL_ThermoMotorIOT_Cad/DAO/ConexaoBD.cs
@@ -9,9 +9,8 @@ namespace DAO
         /// </summary> 
         /// <returns>Conexão aberta</returns> 
         public static SqlConnection GetConexao()
-        {
-            //string strCon = "Data Source=LOCALHOST; Database=AULADB; user id=sa; password=123456";
-            string strCon = "Data Source=DESKTOP-FTRI8H1\\MSSQLSERVER1; Database=AULADB; user id=sa; password=123456";
+        {            
+            string strCon = "Data Source=localhost; Database=PBL; user id=sa; password=123456";
             SqlConnection conexao = new SqlConnection(strCon);
             conexao.Open();
             return conexao;
