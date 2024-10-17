57406be43300e74e60d4d9b4b010ebf241f79a34
99Barros <110702690+99Barros@users.noreply.github.com>
2024-10-17 20:48:43 -0300

    Update ScriptCriaBancoPBL.sql

--- ScriptCriaBancoPBL.sql
+++ ScriptCriaBancoPBL.sql
@@ -1,3 +1,4 @@
+--> TABLES
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'PBL')
 BEGIN
     CREATE DATABASE PBL;
@@ -7,7 +8,7 @@ USE PBL;
 GO
 
 CREATE TABLE Usuarios (
-    IdUsuario INT PRIMARY KEY IDENTITY(1,1),
+    Id INT PRIMARY KEY IDENTITY(1,1),
 	Login NVARCHAR(50) UNIQUE NOT NULL,
     Senha NVARCHAR(255) NOT NULL,
     Nome NVARCHAR(100) NOT NULL,
@@ -18,7 +19,7 @@ CREATE TABLE Usuarios (
 );
 GO
 CREATE TABLE Empresas (
-    IdEmpresa INT PRIMARY KEY IDENTITY(1,1),
+    Id INT PRIMARY KEY IDENTITY(1,1),
     NomeEmpresa NVARCHAR(150) NOT NULL,
     CNPJ NVARCHAR(20) NOT NULL UNIQUE, 
     Endereco NVARCHAR(255) NOT NULL,
@@ -28,7 +29,7 @@ CREATE TABLE Empresas (
 );
 GO
 CREATE TABLE Estufas (
-    IdEstufa INT PRIMARY KEY IDENTITY(1,1),
+    id INT PRIMARY KEY IDENTITY(1,1),
 	IdUsuario INT NOT NULL,
     IdEmpresa INT NOT NULL,    
     Modelo NVARCHAR(50) NOT NULL,    
@@ -39,3 +40,100 @@ CREATE TABLE Estufas (
     FOREIGN KEY (IdEmpresa) REFERENCES Empresas(IdEmpresa) ON DELETE CASCADE,
 	FOREIGN KEY (IdUsuario) REFERENCES Usuarios(IdUsuario) ON DELETE CASCADE
 );
+
+--> PROCEDURES GENERICAS
+
+create procedure spDelete
+(
+ @id int ,
+ @tabela varchar(max)
+)
+as
+begin
+ declare @sql varchar(max);
+ set @sql = ' delete ' + @tabela +
+ ' where id = ' + cast(@id as varchar(max))
+ exec(@sql)
+end
+GO
+
+create procedure spConsulta
+(
+ @id int ,
+ @tabela varchar(max)
+)
+as
+begin
+ declare @sql varchar(max);
+ set @sql = 'select * from ' + @tabela +
+ ' where id = ' + cast(@id as varchar(max))
+ exec(@sql)
+end
+GO
+
+create procedure spListagem
+(
+ @tabela varchar(max),
+ @ordem varchar(max))
+as
+begin
+ exec('select * from ' + @tabela +
+ ' order by ' + @ordem)
+end
+GO
+
+create procedure spProximoId
+(@tabela varchar(max))
+as
+begin
+ exec('select isnull(max(id) +1, 1) as MAIOR from '
+ +@tabela)
+end
+GO
+
+--> PROCEDURES ESPECIFICAS
+CREATE PROCEDURE spInsert_Estufas
+(
+	@id int,
+    @IdUsuario INT,
+    @IdEmpresa INT,
+    @Modelo NVARCHAR(50),
+    @Descricao NVARCHAR(255) = NULL,
+    @Preco MONEY,
+    @PeriodoLocacao INT,
+	@DataCadastro DATETIME
+)
+AS
+BEGIN
+    INSERT INTO Estufas
+    (id,IdUsuario, IdEmpresa, Modelo, Descricao, Preco, PeriodoLocacao, DataCadastro)
+    VALUES
+    (@id, @IdUsuario, @IdEmpresa, @Modelo, @Descricao, @Preco, @PeriodoLocacao, @DataCadastro)
+END
+GO
+
+CREATE PROCEDURE spUpdate_Estufas
+(
+    @id INT,
+    @IdUsuario INT,
+    @IdEmpresa INT,
+    @Modelo NVARCHAR(50),
+    @Descricao NVARCHAR(255) = NULL,
+    @Preco MONEY,
+    @PeriodoLocacao INT,
+    @DataCadastro DATETIME
+)
+AS
+BEGIN
+    UPDATE Estufas
+    SET 
+        IdUsuario = @IdUsuario,
+        IdEmpresa = @IdEmpresa,
+        Modelo = @Modelo,
+        Descricao = @Descricao,
+        Preco = @Preco,
+        PeriodoLocacao = @PeriodoLocacao,
+        DataCadastro = @DataCadastro
+    WHERE id = @id;
+END
+GO
