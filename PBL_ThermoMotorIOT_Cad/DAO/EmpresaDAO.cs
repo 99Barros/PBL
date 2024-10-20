f42be42f065c7c4edf92b949e2c59a95a6b5f12f
Caiozin24 <149202077+Caiozin24@users.noreply.github.com>
2024-10-20 19:22:19 -0300

    Update EmpresaDAO.cs

--- PBL_ThermoMotorIOT_Cad/DAO/EmpresaDAO.cs
+++ PBL_ThermoMotorIOT_Cad/DAO/EmpresaDAO.cs
@@ -5,56 +5,9 @@ using DAO;
 
 namespace PBL_ThermoMotorIOT_Cad.DAO
 {
-    public class EmpresaDAO
+    public class EmpresaDAO : PadraoDAO<EmpresaViewModel>
     {
-        public static void Insert(EmpresaViewModel empresa)
-        {
-            SqlConnection connection = ConexaoBD.GetConexao();
-            empresa.DataCadastro = DateTime.Now;
-            string sql = "INSERT INTO empresas values (@NomeEmpresa, @CNPJ, @Endereco, @Telefone, @Email, @DataCadastro)";
-            HelperDAO.ExecutaSql(sql, CreateParameters(empresa));
-        }
-        public static void Update(EmpresaViewModel empresa)
-        {
-            SqlConnection connection = ConexaoBD.GetConexao();
-            string sql = "UPDATE empresas set NomeEmpresa=@NomeEmpresa, CNPJ=@CNPJ, Endereco=@Endereco, Telefone=@Telefone," +
-                " Email=@Email, DataCastro=@DataCastro WHERE IdEmpresa = @IdEmpresa";
-            HelperDAO.ExecutaSql(sql, CreateParameters(empresa));
-        }
-        public static void Delete(int id)
-        {
-            SqlConnection connection = ConexaoBD.GetConexao();
-            string sql = "DELETE FROM empresas WHERE IdEmpresa=" + id;
-            HelperDAO.ExecutaSql(sql, null);
-        }
-        public EmpresaViewModel Search(int id)
-        {
-            string sql = "select * from empresas where IdEmpresa = " + id;
-            DataTable tabela = HelperDAO.ExecutaSql(sql, null);
-            if (tabela.Rows.Count == 0)
-                return null;
-            else
-                return BuildModel(tabela.Rows[0]);
-        }
-        public static List<EmpresaViewModel> AllSearch()
-        {
-            string sql = "select * from empresas order by IdEmpresa";
-            DataTable tabela = HelperDAO.ExecutaSql(sql, null);
-            if (tabela.Rows.Count == 0)
-                return null;
-            else
-                return BuildTable(tabela);
-        }
-        public static List<EmpresaViewModel> BuildTable(DataTable tabela)
-        {
-            List<EmpresaViewModel> model = new List<EmpresaViewModel>();
-
-            foreach (DataRow registro in tabela.Rows)
-            {
-                model.Add(BuildModel(registro));
-            }
-            return model;
-        }
+        
         private static EmpresaViewModel BuildModel(DataRow registro)
         {
             EmpresaViewModel model = new EmpresaViewModel();
@@ -67,12 +20,6 @@ namespace PBL_ThermoMotorIOT_Cad.DAO
             model.DataCadastro = Convert.ToDateTime(registro["DataCadastro"]);
             return model;
         }
-        public int ProximoId()
-        {
-            string sql = "select isnull(max(IdEmpresa) +1, 1) as 'MAIOR' from empresas";
-            DataTable tabela = HelperDAO.ExecutaSql(sql, null);
-            return Convert.ToInt32(tabela.Rows[0]["MAIOR"]);
-        }
         private static SqlParameter[] CreateParameters(EmpresaViewModel empresa)
         {
             SqlParameter[] parameters = new SqlParameter[7];
@@ -84,6 +31,12 @@ namespace PBL_ThermoMotorIOT_Cad.DAO
             parameters[5] = new SqlParameter("Email", empresa.Email != null ? empresa.Email : DBNull.Value);
             parameters[6] = new SqlParameter("DataCadastro", empresa.DataCadastro);
             return parameters;
+        }
+         protected override void SetTabela()
+        {
+            Tabela = "Empresas";
+            /*NomeSpListagem = "spListagemEmpresas";  
+            conferir se a procedure precisa de join*\
         }
     }
 }
