e994bea77b034f64922e589097a152a099ec6477
Joao Barros <joaoy83@gmail.com>
2024-11-13 19:58:55 -0300

    Adicionando CEP e outros atributos na empresa

--- PBL_ThermoMotorIOT_Cad/DAO/EmpresaDAO.cs
+++ PBL_ThermoMotorIOT_Cad/DAO/EmpresaDAO.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.SqlClient;
+﻿﻿using System.Data.SqlClient;
 using System.Data;
 using PBL_ThermoMotorIOT_Cad.Models;
 using DAO;
@@ -13,22 +13,29 @@ namespace PBL_ThermoMotorIOT_Cad.DAO
             model.id = Convert.ToInt32(registro["Id"]);
             model.NomeEmpresa = registro["NomeEmpresa"].ToString();
             model.CNPJ = registro["CNPJ"].ToString();
-            model.Endereco = registro["Endereco"].ToString();
+            model.CEP = registro["CEP"].ToString();
+            model.Logradouro = registro["Logradouro"].ToString();
+            model.Cidade = registro["Cidade"].ToString();
+            model.Estado = registro["Estado"].ToString();
             model.Telefone = registro["Telefone"].ToString();
             model.Email = registro["Email"].ToString();
             model.DataCadastro = Convert.ToDateTime(registro["DataCadastro"]);
+
             return model;
         }
         protected override SqlParameter[] CreateParameters(EmpresaViewModel empresa)
         {
-            SqlParameter[] parameters = new SqlParameter[7];
+            SqlParameter[] parameters = new SqlParameter[10];
             parameters[0] = new SqlParameter("Id", empresa.id);
             parameters[1] = new SqlParameter("NomeEmpresa", empresa.NomeEmpresa);
             parameters[2] = new SqlParameter("CNPJ", empresa.CNPJ);
-            parameters[3] = new SqlParameter("Endereco", empresa.Endereco);
-            parameters[4] = new SqlParameter("Telefone", empresa.Telefone != null ? empresa.Telefone : DBNull.Value);
-            parameters[5] = new SqlParameter("Email", empresa.Email != null ? empresa.Email : DBNull.Value);
-            parameters[6] = new SqlParameter("DataCadastro", empresa.DataCadastro);
+            parameters[3] = new SqlParameter("CEP", empresa.CEP);
+            parameters[4] = new SqlParameter("Logradouro", empresa.Logradouro);
+            parameters[5] = new SqlParameter("Cidade", empresa.Cidade);
+            parameters[6] = new SqlParameter("Estado", empresa.Estado);
+            parameters[7] = new SqlParameter("Telefone", empresa.Telefone != null ? empresa.Telefone : DBNull.Value);
+            parameters[8] = new SqlParameter("Email", empresa.Email != null ? empresa.Email : DBNull.Value);
+            parameters[9] = new SqlParameter("DataCadastro", empresa.DataCadastro);
             return parameters;
         }
 
