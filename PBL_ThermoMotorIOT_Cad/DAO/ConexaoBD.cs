16358f925f00037a527b1d1321330d9b38ca8bdb
FELIPE KENZO OZAKO <felipeozako@gmail.com>
2024-11-20 19:30:45 -0300

    Change: GetConexao to localhost

--- PBL_ThermoMotorIOT_Cad/DAO/ConexaoBD.cs
+++ PBL_ThermoMotorIOT_Cad/DAO/ConexaoBD.cs
@@ -10,8 +10,7 @@ namespace DAO
         /// <returns>Conexão aberta</returns> 
         public static SqlConnection GetConexao()
         {
-            //string strCon = "Data Source=localhost; Database=PBL; user id=sa; password=123456";
-            string strCon = "Data Source=DESKTOP-FTRI8H1\\MSSQLSERVER02; Database=PBL; user id=sa; password=123456";
+            string strCon = "Data Source=localhost; Database=PBL; user id=sa; password=123456";
             SqlConnection conexao = new SqlConnection(strCon);
             conexao.Open();
             return conexao;
