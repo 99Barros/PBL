4fdc23540e62b019ba923968d4c1b201c49e1a37
Kaiki Fischer <kaiki.fischer@hotmail.com>
2024-11-25 20:17:42 -0300

    Feat: Alteração no dashboard clean.

---
+++ PBL_ThermoMotorIOT_Cad/wwwroot/js/dashboard.js
@@ -0,0 +1,22 @@
+﻿// Configuração do gráfico
+window.onload = function () {
+    const chart = new CanvasJS.Chart("chartContainer", {
+        theme: "light1", // Opções: "light2", "dark1", "dark2"
+        animationEnabled: true,
+        title: {
+            text: "Total de Cadastros"
+        },
+        axisY: {
+            interval: 1, // Define o intervalo de valores no eixo Y
+            title: "Quantidade",
+            includeZero: true // Garante que o eixo Y comece do zero
+        },
+        data: [
+            {
+                type: "column", // Gráfico de colunas
+                dataPoints: dataPoints
+            }
+        ]
+    });
+    chart.render();
+};
